37a01f262de6d1fe3c124c268f7aca1c2a27a07a
RobinDarley <rhoward78130@gmail.com>
2015-09-19 10:44:03 -0500

    Finished prototype

--- prototype/js/proto.js
+++ prototype/js/proto.js
@@ -5,11 +5,22 @@ $(document).ready(function() {
     var p2 = $('#page2');
     var p3 = $('#page3');
     var p4 = $('#page4');
+    
+    var back = $('#back');
+    var settings = $('#settingsBtn');
+    var back2 = $('#backToProfile');
     var scroller = $('#pageContainer');
+    var mainAv = $('#mainAv');
     
     $('.loading').delay(1500).fadeOut('slow');
     
     p1.click(function(){
+        scroller.animate({
+            left: '0'
+        });
+    });
+    
+    back.click(function(){
         scroller.animate({
             left: '0'
         });
@@ -27,7 +38,25 @@ $(document).ready(function() {
         });
     });
     
+    mainAv.click(function(){
+        scroller.animate({
+            left: '-1080px'
+        });
+    });
+    
+    back2.click(function(){
+        scroller.animate({
+            left: '-1080px'
+        });
+    });
+    
     p4.click(function(){
+        scroller.animate({
+            left: '-1620px'
+        });
+    });
+    
+    settings.click(function(){
         scroller.animate({
             left: '-1620px'
         });
